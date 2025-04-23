70add17beb1fa0305bc9e136fb88933759a2470b
Alise <ipa23.k.strazdina@vtdt.edu.lv>
2025-04-23 09:46:32 +0300

    ajnfeskjfs

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'role',
+        'password',
+    ];
+
+    
 }
