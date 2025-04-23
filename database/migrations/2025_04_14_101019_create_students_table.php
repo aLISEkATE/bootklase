84183a31ae1ed8346a13cd3ea348c5de5c9027e3
Alise <ipa23.k.strazdina@vtdt.edu.lv>
2025-04-23 09:22:22 +0300

    added new database code for students

--- database/migrations/2025_04_14_101019_create_students_table.php
+++ database/migrations/2025_04_14_101019_create_students_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['student', 'teacher'])->default('student');
             $table->timestamps();
         });
     }
